235db42e7c405a53b44e1c388e4f3a396dd51d0d
Chris King <kingces95@gmail.com>
2016-09-04 12:37:24 -0700

    Add NUnit.Runners

---
+++ packages/NUnit.Runners.2.6.4/tools/runpnunit.bat
@@ -0,0 +1,6 @@
+version https://git-lfs.github.com/spec/v1
+archive.path tools/runpnunit.bat
+archive.type zip
+archive.url http://nuget.org/api/v2/package/NUnit.Runners/2.6.4
+oid sha256:78a2d37826ad9be173977f9e7bdbb698e1eda8488b3e102d790256c3cda7bfe0
+size 81
